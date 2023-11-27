e3fef6282b74f80e46bc77dbeba149ae533f0db0
Ali Anwar <alianwarofc@gmail.com>
2023-11-27 23:27:16 +0700

    font bold

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -2,7 +2,7 @@
 
 export const Headers = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between font-bold">
         <a href="#">My Notes</a>
         <a href="#">Menu</a>
     </div>
